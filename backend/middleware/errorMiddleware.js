00b246998cf348370b24834c64dac3c98c09ce82
huanNguyen2204 <huan.nc2204@sinhvien.hoasen.edu.vn>
2023-12-24 21:08:22 +0700

    finish

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,9 @@
+const errorHandler = (req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500
+  res.status(statusCode)
+  res.json(statusCode)
+}
+
+module.exports = {
+  errorHandler
+}
